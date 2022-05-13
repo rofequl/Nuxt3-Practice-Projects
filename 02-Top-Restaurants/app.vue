f5a12239f6aab704a444da001a7f40892440e74b
rofequlislam <rofequlislamnayem@gmail.com>
2022-05-13 09:39:18 +0600

    New Top Restaurants project start

---
+++ 02-Top-Restaurants/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <button type="button" class="btn btn-primary">Primary</button>
+  </div>
+</template>
---
+++ 02-Top-Restaurants/nuxt.config.ts
@@ -0,0 +1,13 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+    meta: {
+        link: [
+            {
+                rel: "stylesheet",
+                href: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+            }
+        ]
+    }
+})
